784e00e48e7fa1f5876e056e8151e281143b291e
Dilum Navanjana <dilumnavanjana@gmail.com>
2022-03-21 21:57:11 +0100

    database migration fix

--- db/migrate/20220320182226_organizations_table.rb
+++ db/migrate/20220320182226_organizations_table.rb
@@ -1,9 +1,9 @@
 class OrganizationsTable < ActiveRecord::Migration[7.0]
   def change
-    create_table :organiations do |t|
+    create_table :organizations do |t|
       t.string :name
       t.string :logo
-      t.boolean :pegged
+      t.boolean :pegged, default: false
 
       t.timestamps
     end
