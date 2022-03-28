8a9b991e4febdeaa0879625c9d27cdf34271a08f
Dilum Navanjana <dilumnavanjana@gmail.com>
2022-03-28 10:00:44 +0200

    fix empty string check in remarks_display

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -7,9 +7,9 @@ class Organization < ApplicationRecord
   def remarks_display
     text = ''
 
-    text += "<b>Initial salary pegging value</b> - #{pegging_value} #{pegged_currency}<br>" if pegging_value
-    text += "<b>Salary proportion</b> - #{proportion}<br>" if proportion
-    text += "<b>Additional remarks</b> - #{remarks}" if remarks
+    text += "<b>Initial salary pegging value</b> - #{pegging_value} #{pegged_currency}<br>" if pegging_value.present?
+    text += "<b>Salary proportion</b> - #{proportion}<br>" if proportion.present?
+    text += "<b>Additional remarks</b> - #{remarks}" if remarks.present?
 
     text
   end
