933724be2e75e3f890f373752e5580a74f821b04
Dilum Navanjana <dilumnavanjana@gmail.com>
2022-03-29 08:09:24 +0200

    only allow change existing data changes via form & adding new company data need to go via Github

--- app/models/change_request.rb
+++ app/models/change_request.rb
@@ -3,8 +3,7 @@ class ChangeRequest < ApplicationRecord
   include ProportionEnum
 
   enum request_type: {
-    change_existing_details: 'change_existing_details',
-    add_new_data: 'add_new_data'
+    change_existing_details: 'change_existing_details'
   }
 
   validates :request_type, :company_name, :pegged_currency, presence: true
