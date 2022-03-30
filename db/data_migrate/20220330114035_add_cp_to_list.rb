70905518ce5dc6dfb75c3830ba23e72fa76de460
Moin Khan <kmoin786@gmail.com>
2022-03-30 17:32:22 +0530

    Update great_place_to_work value

--- db/data_migrate/20220330114035_add_cp_to_list.rb
+++ db/data_migrate/20220330114035_add_cp_to_list.rb
@@ -3,7 +3,7 @@ class AddCpToList < ActiveRecord::Migration[7.0]
     Organization.create!(
       name: "Capital Placement",
       pegged: true,
-      great_place_to_work: true,
+      great_place_to_work: false,
       pegged_currency: 'GBP',
       remarks: 'Our base salary starts at £350 for all new employees. Existing employees will get the GBP salary that we paid in February 2022'
     )
