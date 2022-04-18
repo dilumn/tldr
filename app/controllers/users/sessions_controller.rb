968b7c681f4226dfc922d4450e17f7dbf3227f06
Dilum Navanjana <dilumnavanjana@gmail.com>
2022-04-18 21:23:55 +0200

    routes.rb to support different roots depend on the domain

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(_resource_or_scope)
-    root_path
+    peggedplacetowork_root_path
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || root_path
+    stored_location_for(resource_or_scope) || peggedplacetowork_root_path
   end
 end
