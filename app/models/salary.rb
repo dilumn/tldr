5c2334b15c0eed14a604b4c19c3396cd31dc0205
Dilum Navanjana <dilumnavanjana@gmail.com>
2022-06-01 17:33:31 +0200

    dependent option fix

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -29,7 +29,7 @@ class Salary < ApplicationRecord
     rejected: 'rejected'
   }
 
-  has_many :salary_votes, dependent: true
+  has_many :salary_votes, dependent: :destroy
 
   def accurate_votes_count
     salary_votes.count(&:accurate?)
