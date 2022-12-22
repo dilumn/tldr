a95f3591d6a0646786e78ade5664b569bdb93300
dilumn <dilumnavanjana@gmail.com>
2022-12-22 23:44:37 +0100

    removed unwanted logics inside tldr index

--- app/controllers/tldr_main_controller.rb
+++ app/controllers/tldr_main_controller.rb
@@ -3,9 +3,5 @@
 class TldrMainController < ApplicationController
 
   def index
-    salaries = Salary.includes(:salary_votes).where(status: 'approved').group_by(&:designation)
-    @grouped_salaries = salaries.sort_by do |_key, value|
-      -value.size
-    end.to_h
   end
 end
