b23281e77c924d54debe1d5313e551e53af49e19
dilumn <dilumnavanjana@gmail.com>
2022-12-23 18:26:00 +0100

    epf, etf added

--- app/controllers/thetax_controller.rb
+++ app/controllers/thetax_controller.rb
@@ -3,13 +3,22 @@
 class ThetaxController < ApplicationController
 
   def index
-    @tax = Tax.new(current_salary: session[:current_salary], tax_amount: session[:tax_amount])
+    @tax = Tax.new(
+      current_salary: session[:current_salary],
+      tax_amount: session[:tax_amount],
+      epf: session[:epf],
+      etf: session[:etf],
+      take_home: session[:take_home]
+    )
   end
 
   def calculate
     calculated_tax = Tax.new(current_salary: params[:tax][:current_salary].to_i).calculate
     session[:current_salary] = calculated_tax.current_salary
     session[:tax_amount] = calculated_tax.tax_amount
+    session[:epf] = calculated_tax.epf
+    session[:etf] = calculated_tax.etf
+    session[:take_home] = calculated_tax.take_home
 
     redirect_to thetax_root_path
   end
